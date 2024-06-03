74722c9a7a85c42fbb7fbf0f68d583e54ed62a94
amansharma59 <aman.sharma@costrategix.com>
2024-06-03 18:51:53 +0530

    get profile by user ID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -95,4 +95,27 @@ router.post(
   }
 );
 
+// @route       GET api/profile/user/:user_id
+// @desc        Get profile by user id
+// @access      Public
+
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.find({ user: req.params.user_id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
+    if (!profile) {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == "ObjectId") {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
